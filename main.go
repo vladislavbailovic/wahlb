9ce54fd6e14c00b875c71716cb00a09c2ca54f7b
vladislav <ve@incsub.com>
2022-12-18 05:56:51 +0100

    Find main item using goquery

--- main.go
+++ main.go
@@ -1,24 +1,36 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"wahlb/internal"
 
 	md "github.com/JohannesKaufmann/html-to-markdown"
+	"github.com/PuerkitoBio/goquery"
 	"github.com/charmbracelet/glamour"
 )
 
 func main() {
 	b := internal.GetTestFile("su.html")
-	conveter := md.NewConverter("", true, nil)
-	mdown, err := conveter.ConvertString(string(b))
+	buffer := bytes.NewBuffer(b)
+
+	doc, err := goquery.NewDocumentFromReader(buffer)
 	if err != nil {
-		fmt.Println("Error:", err)
+		panic(err)
 	}
+
+	conveter := md.NewConverter("", true, nil)
+	/*
+		mdown, err := conveter.ConvertString(doc.Find("body"))
+		if err != nil {
+			panic(err)
+		}
+	*/
+	mdown := conveter.Convert(doc.Find("[itemprop='mainEntity']"))
 
 	out, err := glamour.Render(mdown, "dark")
 	if err != nil {
-		fmt.Println("Error:", err)
+		panic(err)
 	}
 
 	fmt.Println(out)
